9eab9b0e4bb15aead2bce3a8d66c66498d51f820
Vegeta <183507328@qq.com>
2019-07-25 20:29:04 +0800

    测试一下，现在是提交到本地仓库

---
+++ test_git/src/main/java/com/itheima/test/TestGit.java
@@ -0,0 +1,13 @@
+package com.itheima.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestGit {
+    public static void main(String[] args) {
+        /**
+         * 我是开发者A,我增加了下面这段代码！
+         */
+        Map<String, Object> map = new HashMap<String, Object>();
+    }
+}
